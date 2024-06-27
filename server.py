ec14d293d586ecf80040d4baf221cc8dd59852e3
sierraflores <sierranf97@gmail.com>
2024-06-27 13:51:14 -0500

    Complete links/pathways for subpages. Refine appearance of homepage. Add content to footer.

--- server.py
+++ server.py
@@ -29,5 +29,25 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+
+@app.route('/newsfeed')
+def news():
+    return render_template("newsfeed.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
